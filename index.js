f38cf317629ed7bbd75e46bc53848903c7a7a447
Rupesh kumar <rupeshbuddhu78@gmail.com>
2026-04-14 22:27:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const https = require('https'); // 🔥 Naya import
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
